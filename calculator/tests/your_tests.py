cf2010a7535d5fcab46dbe1b5be040595695c737
Rafael Perez <rafaelperez@DNa1c46b0.SUNet>
2023-05-14 16:53:01 -0700

    Added Makefile and Py tests

--- calculator/tests/your_tests.py
+++ calculator/tests/your_tests.py
@@ -3,8 +3,8 @@ from calculator_adapter import run
 
 
 ### ADD AT LEAST TWO TESTS HERE!
-
-
+assert run("2 + 2").output == "4"
+assert run("2 * 2").exit_status == 0
 ###
 
 print("All tests passed!")
